b31c150af8e9b0bc050bbeaa795b9305f39208e2
Alexander Veats <ajveats@gmail.com>
2020-10-23 12:54:45 +0100

    Added an index.html page to the website along with css files index.css and navigationBar.css. The css files have been added to a new directory in the home directory. An image directory has been added in the home directory along with an image for the index.html.

--- fitnesswebapp/myproject/home/views.py
+++ fitnesswebapp/myproject/home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'home/index.html')
