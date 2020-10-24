ce1b6a9a18a76feb8747bb407a3e7caee995653e
Tim <tsgdev@icloud.com>
2020-10-24 15:44:44 +0100

    Adding the Login and Sign Up HTML/CSS/JS

---
+++ fitnesswebapp/myproject/home/static/home/js/loginSignUp.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("login");
+var y = document.getElementById("signup");
+var z = document.getElementById("button");
+
+function signup() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
