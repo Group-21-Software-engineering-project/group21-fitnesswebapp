501c63184a62b46191f668bdc059d07b34537d2d
CainBurt <cain.m.burt@gmail.com>
2020-10-26 19:29:20 +0000

    Sign up
    New users can sign up and are added.

--- fitnesswebapp/myproject/users/views.py
+++ fitnesswebapp/myproject/users/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm #imports django forms
+from django.contrib import messages
+from .forms import UserSignUpForm
+
 
 # Create your views here.
+
+#validates form and sends sucess message + redirects to home page if sucessful
 def signUp(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home-page')
+    else:
+        form = UserSignUpForm()
     return render(request, 'users/signUp.html',{'form': form})
