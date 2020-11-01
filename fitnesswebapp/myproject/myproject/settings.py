8cba88fae3f7f78ac55671053145ad176ac73716
Alexander <ajveats@gmail.com>
2020-11-01 19:01:09 +0000

    Users can now access the body stats page

--- fitnesswebapp/myproject/myproject/settings.py
+++ fitnesswebapp/myproject/myproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'home.apps.HomeConfig',
+    'bodyStats.apps.BodyStatsConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
