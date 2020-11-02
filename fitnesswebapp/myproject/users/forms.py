0b7cd07f75f041c7d9c16191007a42825ae06846
tsgxo <timothy_griffiths@hotmail.co.uk>
2020-11-02 15:47:57 +0000

    Added a hover question mark

--- fitnesswebapp/myproject/users/forms.py
+++ fitnesswebapp/myproject/users/forms.py
@@ -10,3 +10,9 @@ class UserSignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
