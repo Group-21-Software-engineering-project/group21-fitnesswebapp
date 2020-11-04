8ca5a353ce4c45079f671cc1f80de32ec0ff4a68
CainBurt <cain.m.burt@gmail.com>
2020-11-04 10:37:49 +0000

    Password Inputs
    Removed password inputs from the user creation form

--- fitnesswebapp/myproject/users/forms.py
+++ fitnesswebapp/myproject/users/forms.py
@@ -7,12 +7,23 @@ from django.contrib.auth.forms import UserCreationForm
 class UserSignUpForm(UserCreationForm):
     email = forms.EmailField()
 
+    #define passwords to remove the help text
+    password1 = forms.CharField(label='Enter password', 
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', 
+                                widget=forms.PasswordInput)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        #removes help text from the body of the form. Its added under the ? icon
+        help_texts = {
+            "username":None,
+            "email":None,
+            "password1":None,
+            "password2":None,
+        }
 
 def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
 
-        for fieldname in ['username', 'email', 'password1', 'password2']:
-            self.fields[fieldname].help_text = None
