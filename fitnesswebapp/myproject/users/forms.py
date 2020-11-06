b811118c1409ab8a52c04f2f15385b533dd9d41d
CainBurt <cain.m.burt@gmail.com>
2020-11-06 14:45:16 +0000

    Profile
    Added so that profile is created when a new account is created. Removed help text from create account form

--- fitnesswebapp/myproject/users/forms.py
+++ fitnesswebapp/myproject/users/forms.py
@@ -7,12 +7,21 @@ from django.contrib.auth.forms import UserCreationForm
 class UserSignUpForm(UserCreationForm):
     email = forms.EmailField()
 
+    password1 = forms.CharField(label='Enter Password',
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password',
+                                widget=forms.PasswordInput)
+
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
+        help_texts = {
+            "username":None,
+            "email":None,
+            "password1":None,
+            "password2":None,
+        }
+        
 def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
-
-        for fieldname in ['username', 'email', 'password1', 'password2']:
-            self.fields[fieldname].help_text = None
