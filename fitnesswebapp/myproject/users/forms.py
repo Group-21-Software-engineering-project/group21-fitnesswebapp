131d8490604ec188158c565a69b415e7d11c6a3a
CainBurt <cain.m.burt@gmail.com>
2020-11-06 15:15:58 +0000

    Profile Edit
    added profile editing functionality. can change email and password

--- fitnesswebapp/myproject/users/forms.py
+++ fitnesswebapp/myproject/users/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-
 #creating a form with extra fields. ie email
 class UserSignUpForm(UserCreationForm):
     email = forms.EmailField()
@@ -22,6 +21,19 @@ class UserSignUpForm(UserCreationForm):
             "password1":None,
             "password2":None,
         }
-        
+
 def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
+
+
+#update username and email
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        help_texts = {
+            "username":None,
+            "email":None,
+        }
