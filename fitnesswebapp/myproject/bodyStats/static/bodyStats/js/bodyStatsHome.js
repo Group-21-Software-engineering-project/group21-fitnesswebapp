9a1e963d76c8b8ac906910b8336799579436b383
Alexander <ajveats@gmail.com>
2020-11-08 15:03:03 +0000

    Continued working on bodyStatsHome.js and bodyStatsHome.html - Added validation to dates in bodyStatsHome.html - Added validation to other fields in bodyStatsHome.js

--- fitnesswebapp/myproject/bodyStats/static/bodyStats/js/bodyStatsHome.js
+++ fitnesswebapp/myproject/bodyStats/static/bodyStats/js/bodyStatsHome.js
@@ -1,9 +1,13 @@
-function submitBodyStats () {
+function validateBodyStats () {
     var bodyWeight = document.getElementById("bodyStatWeight").value;
     var bodyWeightUnit = document.getElementById("unitWeight").value;
     var bodyHeight = document.getElementById("bodyStatHeight").value;
     var bodyHeightUnit = document.getElementById("unitHeight").value;
     var bodyStatsDate = document.getElementById("bodyStatDate").value;
+
+    console.log("Weight: " + bodyWeight + " " + bodyHeightUnit);
+    console.log("Height: " + bodyHeight + " " + bodyWeightUnit);
+    console.log("Date: " + bodyStatsDate);
 
     if (bodyWeightUnit === "lb") {
         var bodyWeightTemp = (bodyWeight * 2.205);
@@ -22,4 +26,6 @@ function submitBodyStats () {
     if (bodyWeight <= 35 || bodyWeight >= 200) {
         alert("Please enter in a valid weight");
     }
+
+
 }
