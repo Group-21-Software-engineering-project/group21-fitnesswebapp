3d20b9e30a781165baf5d1dfe21163fbb6bf9927
Alexander <ajveats@gmail.com>
2020-11-09 21:26:10 +0000

    Continued work on update user body stats with bodyStatsHome.js - Users can now input they weight and height into bodyStatsHome.html which is then sent to the bodyStatsHome.js, where the users input is validated to ensure that they have entered correct value. The users measurement aer converted from imperial to metric units if the have chosen to enter their measurements as imperial. The users BMI is then calculated with the users body stats. The users updated body stats are then turned into an object and then further turned into a JSON object, for it to be sent to the backend.

--- fitnesswebapp/myproject/bodyStats/static/bodyStats/js/bodyStatsHome.js
+++ fitnesswebapp/myproject/bodyStats/static/bodyStats/js/bodyStatsHome.js
@@ -1,21 +1,29 @@
-function validateBodyStats () {
-    var bodyWeight = document.getElementById("bodyStatWeight").value;
-    var bodyWeightUnit = document.getElementById("unitWeight").value;
-    var bodyHeight = document.getElementById("bodyStatHeight").value;
-    var bodyHeightUnit = document.getElementById("unitHeight").value;
-    var bodyStatsDate = document.getElementById("bodyStatDate").value;
+/**
+ *
+ * @function updateUserBodyStats()
+ * This function get the data that the user has entered into the update body stats forms in bodyStatsHome.html.
+ * To test that the js has gotten the correct values in the correct format, the users input is outputted to the console.
+ * If the user has selected to to input their body stats
+ *
+ */
 
-    console.log("Weight: " + bodyWeight + " " + bodyHeightUnit);
-    console.log("Height: " + bodyHeight + " " + bodyWeightUnit);
-    console.log("Date: " + bodyStatsDate);
+function updateUserBodyStats() {
+
+    let bodyWeight = document.getElementById('updateBodyStats').elements.namedItem("bodyStatWeight").value;
+    const bodyWeightUnit = document.getElementById('updateBodyStats').elements.namedItem("unitWeight").value;
+    let bodyHeight = document.getElementById('updateBodyStats').elements.namedItem("bodyStatHeight").value;
+    const bodyHeightUnit = document.getElementById('updateBodyStats').elements.namedItem("unitHeight").value;
+    const bodyStatsDate = document.getElementById('updateBodyStats').elements.namedItem("bodyStatDate").value;
+
+    // console.log("Weight: " + bodyWeight + " " + bodyHeightUnit + "\nHeight: " + bodyHeight + " " + bodyWeightUnit + "\nDate: " + bodyStatsDate);
 
     if (bodyWeightUnit === "lb") {
-        var bodyWeightTemp = (bodyWeight * 2.205);
+        const bodyWeightTemp = (bodyWeight * 2.205);
         bodyWeight = bodyWeightTemp.toFixed(2);
     }
 
     if (bodyHeightUnit === "inches") {
-        var bodyHeightTemp = (bodyHeight * 2.54);
+        const bodyHeightTemp = (bodyHeight * 2.54);
         bodyHeight = bodyHeightTemp.toFixed(2);
     }
 
@@ -27,5 +35,43 @@ function validateBodyStats () {
         alert("Please enter in a valid weight");
     }
 
+    const bodyStatsBMI = calculateBMI(bodyHeight, bodyWeight);
+    //console.log(bodyStatsBMI);
+
+    let updatedBodyStats = {
+        "weight": bodyWeight,
+        "height": bodyHeight,
+        "date": bodyStatsDate,
+        "BMI": bodyStatsBMI
+    };
+
+    let updatedBodyStatsJSON = JSON.stringify(updatedBodyStats);
+
+    console.log(updatedBodyStats);
+    console.log(updatedBodyStatsJSON);
 
 }
+
+/**
+ *
+ * @function calculateBMI()
+ * This functions takes in the user height and weight as parameters. It then converts the users height from centimeters
+ * to meters by dividing the users height by 100. It then calculates the users BMI by dividing the users body weight
+ * by their height squared, and then round to two decimals places.
+ *
+ * @param bodyHeight - the height of the user in centimeters
+ * @param bodyWeight - the weight of the user in kilograms
+ *
+ * @return {number} BMI - the BMI of the user
+ *
+ */
+
+function calculateBMI(bodyHeight, bodyWeight) {
+    //console.log(bodyHeight);
+    //console.log(bodyWeight);
+    const heightMeters = (bodyHeight / 100);
+    const BMI = (bodyWeight / (heightMeters * heightMeters)).toFixed(2);
+    //console.log("Height: " + bodyHeight + "\nWeight: " + bodyWeight + "\nBMI: " + BMI);
+    return BMI;
+}
+
