68ac7118237b8cb91ffb5fc7882168b1cdeefbcd
Alexander Veats <ajveats@gmail.com>
2020-11-15 18:01:46 +0000

    General working on body stats and the food tracker application - Allowing users data to be saved and returned from the database

--- fitnesswebapp/myproject/bodyStats/views.py
+++ fitnesswebapp/myproject/bodyStats/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import body_stats_tbl
 from django.shortcuts import render, redirect, get_object_or_404
+from .forms import bodyStatsForm
+from django.contrib import messages
 from django.http import HttpResponse
 
 
@@ -12,15 +14,21 @@ def bodyStats(request):
 
 def updateBodyStats(request):
     if request.method == "POST":
-        if request.POST.get('bodyStatHeight') and request.POST.get('unitHeight') and request.POST.get(
-                'bodyStatWeight') and request.POST.get('unitWeight'):
-            updateStats = body_stats_tbl()
-            updateStats.bodyStatHeight = request.POST.get('bodyStatHeight')
-            updateStats.unitHeight = request.POST.get('unitHeight')
-            updateStats.bodyStatWeight = request.POST.get('bodyStatWeight')
-            updateStats.unitWeight = request.POST.get('unitHeight')
-            updateStats.save()
+        form = bodyStatsForm(request.POST)
+        if form.is_valid():
+            # process data here
+            messages.success(request, f'body stats have been updated')
+            return redirect('bodyStats-page')
 
-            return render(request, 'bodyStats/templates/bodyStatsHome.html')
-        else:
-            return render(request, 'bodyStats/templates/bodyStatsHome.html')
+        #if request.POST.get('bodyStatHeight') and request.POST.get('unitHeight') and request.POST.get(
+        #      'bodyStatWeight') and request.POST.get('unitWeight'):
+        #    updateStats = body_stats_tbl()
+        #    updateStats.bodyStatHeight = request.POST.get('bodyStatHeight')
+        #    updateStats.unitHeight = request.POST.get('unitHeight')
+        #    updateStats.bodyStatWeight = request.POST.get('bodyStatWeight')
+        #    updateStats.unitWeight = request.POST.get('unitHeight')
+        #    updateStats.save()
+
+        #    return render(request, 'bodyStats/templates/bodyStatsHome.html')
+        #else:
+        #    return render(request, 'bodyStats/templates/bodyStatsHome.html')
