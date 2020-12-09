b2b258faca80c6e9e4c65461c31a14eb523bde0c
Alexander Veats <ajveats@gmail.com>
2020-12-09 09:53:10 +0000

    Updated the model file for body stat and food app

--- fitnesswebapp/myproject/bodyStats/models.py
+++ fitnesswebapp/myproject/bodyStats/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 # Create your models here.
 
 # Specifying the body_stats_tbl table for the database so users can store their body stats
+
+
 class body_stats_tbl(models.Model):
-    bs_entry_id = models.AutoField('body stat entry id', primary_key=True, unique=True, db_index=True, null=False)
+    bs_entry_id = models.AutoField('body stat entry id', primary_key=True, unique=True, db_index=True, default=datetime.now, null=False)
     bs_date = models.DateTimeField('Date Recorded')
-    height = models.DecimalField('Height', max_digits=5, decimal_places=2)
-    weight = models.DecimalField('Weight', max_digits=5, decimal_places=2)
+    height = models.DecimalField('Height cm', max_digits=5, decimal_places=2)
+    weight = models.DecimalField('Weight kg', max_digits=5, decimal_places=2)
     bmi = models.DecimalField('BMI', max_digits=4, decimal_places=2)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
--- fitnesswebapp/myproject/food/models.py
+++ fitnesswebapp/myproject/food/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime
 
 # Create your models here.
 
 
 # Specifying the food_tbl table for the database so users can store the food they have eaten
+
+
 class food_tbl(models.Model):
-    food_entry_id = models.AutoField('food entry id', primary_key=True, unique=True, db_index=True, null=False)
+    food_entry_id = models.AutoField('food entry id', default=datetime.now, primary_key=True, unique=True, db_index=True, null=False)
     food_date = models.DateTimeField('date recorded')
     food_name = models.TextField('food name')
     food_calories = models.IntegerField('food calories', validators=[MinValueValidator(1), MaxValueValidator(3000)])
