78edb12aff5f9a64b6478b4166e5b7d7ecd752d9
Alexander Veats <ajveats@gmail.com>
2020-12-10 18:07:40 +0000

    Users are no longer allowed to submit negative value for the body stats upload page

--- fitnesswebapp/myproject/bodyStats/models.py
+++ fitnesswebapp/myproject/bodyStats/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -12,7 +13,7 @@ from datetime import datetime
 class body_stats_tbl(models.Model):
     bs_entry_id = models.AutoField('body stat entry id', primary_key=True, unique=True, db_index=True, null=False)
     bs_date = models.DateTimeField('Date Recorded', default=datetime.now)
-    height = models.DecimalField('Height cm', max_digits=5, decimal_places=2)
-    weight = models.DecimalField('Weight kg', max_digits=5, decimal_places=2)
-    bmi = models.DecimalField('BMI', max_digits=4, decimal_places=2)
+    height = models.DecimalField('Height cm', max_digits=5, decimal_places=2, validators=[MinValueValidator(0)])
+    weight = models.DecimalField('Weight kg', max_digits=5, decimal_places=2, validators=[MinValueValidator(0)])
+    bmi = models.DecimalField('BMI', max_digits=4, decimal_places=2, validators=[MinValueValidator(0)])
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
